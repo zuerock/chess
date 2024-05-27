edb16a2b75f2dd48c23bc16b527aba21d0a078ba
zuerock <tyler.adams@byu.edu>
2024-05-27 15:56:24 -0600

    Broken

---
+++ client/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import chess.*;
+import ui.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Client: " + piece);
+        Menu client = new Menu(8080);
+        client.run();
+    }
+}
