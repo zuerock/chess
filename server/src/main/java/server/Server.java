01f8635806b78c5e6beefb7decc12fad1fa02d92
zuerock <tyler.adams@byu.edu>
2024-06-02 22:29:47 -0600

    Phase 3

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -21,16 +21,16 @@ public class Server {
 
         Spark.port(desiredPort);
 
-        Spark.staticFiles.location("/web");
+        Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.delete("/db", ((request, result) -> new ClearHandler().handle(request, result, userObj, authObj, gameObj)));
-        Spark.post("/user", ((request, result) -> new RegisterHandler().handle(request, result, userObj, authObj)));
-        Spark.post("/session", ((request, result) -> new LoginHandler().handle(request, result, userObj, authObj)));
-        Spark.delete("/session", ((request, result) -> new LogoutHandler().handle(request, result, authObj)));
-        Spark.get("/game", ((request, result) -> new ListGamesHandler().handle(request, result, authObj, gameObj)));
-        Spark.post("/game", ((request, result) -> new CreateGameHandler().handle(request, result, authObj, gameObj)));
-        Spark.put("/game", ((request, result) -> new JoinGameHandler().handle(request, result, authObj, gameObj)));
+        Spark.delete("db", ((request, result) -> new ClearHandler().handle(request, result, userObj, authObj, gameObj)));
+        Spark.post("user", ((request, result) -> new RegisterHandler().handle(request, result, userObj, authObj)));
+        Spark.post("session", ((request, result) -> new LoginHandler().handle(request, result, userObj, authObj)));
+        Spark.delete("session", ((request, result) -> new LogoutHandler().handle(request, result, authObj)));
+        Spark.get("game", ((request, result) -> new ListGamesHandler().handle(request, result, authObj, gameObj)));
+        Spark.post("game", ((request, result) -> new CreateGameHandler().handle(request, result, authObj, gameObj)));
+        Spark.put("game", ((request, result) -> new JoinGameHandler().handle(request, result, authObj, gameObj)));
 
         Spark.awaitInitialization();
         return Spark.port();
