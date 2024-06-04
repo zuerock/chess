9bee2dc277c43d74a1b0be9705254b10140b8bda
zuerock <tyler.adams@byu.edu>
2024-06-04 12:59:36 -0600

    Phase 4.4

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -4,12 +4,28 @@ import model.GameData;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameDAO {
+public abstract class GameDAO {
 
     public List<GameData> gameList = new ArrayList<>();
     public int currentID = 1;
 
+    public abstract void createGame(GameData game);
+
+    public abstract int getCurrentID();
+
+    public abstract void setCurrentID(int i);
+
+    public abstract void removeGame(int index);
+
+    public abstract GameData getGame(int index);
+
+    public abstract void setGame(int index, GameData game);
+
+    public abstract int getSize();
+
     public void clearGameList(){
         gameList.clear();
     }
+
+    public abstract List<GameData> returnGameList();
 }
