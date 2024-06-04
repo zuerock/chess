5ff15db855e43fce57ac834797041bdf8498c561
zuerock <tyler.adams@byu.edu>
2024-06-04 08:28:53 -0600

    Phase 4.1

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -2,12 +2,13 @@ package dataaccess;
 
 import java.sql.*;
 import java.util.Properties;
+import com.google.gson.Gson;
 
 public class DatabaseManager {
-    private static final String DATABASE_NAME;
-    private static final String USER;
-    private static final String PASSWORD;
-    private static final String CONNECTION_URL;
+    private static final String databaseName;
+    private static final String user;
+    private static final String password;
+    private static final String connectionUrl;
 
     /*
      * Load the database information for the db.properties file.
@@ -18,13 +19,13 @@ public class DatabaseManager {
                 if (propStream == null) throw new Exception("Unable to load db.properties");
                 Properties props = new Properties();
                 props.load(propStream);
-                DATABASE_NAME = props.getProperty("db.name");
-                USER = props.getProperty("db.user");
-                PASSWORD = props.getProperty("db.password");
+                databaseName = props.getProperty("db.name");
+                user = props.getProperty("db.user");
+                password = props.getProperty("db.password");
 
                 var host = props.getProperty("db.host");
                 var port = Integer.parseInt(props.getProperty("db.port"));
-                CONNECTION_URL = String.format("jdbc:mysql://%s:%d", host, port);
+                connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
             }
         } catch (Exception ex) {
             throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
@@ -34,13 +35,49 @@ public class DatabaseManager {
     /**
      * Creates the database if it does not already exist.
      */
-    static void createDatabase() throws DataAccessException {
+    public static void createDatabase() throws DataAccessException {
         try {
-            var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
-            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            // Create database
+            var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
             }
+
+            // Access the database
+            conn.setCatalog(databaseName);
+
+            // Create games table
+            String createGamesTable = "CREATE TABLE IF NOT EXISTS games (" +
+                    "ID INT NOT NULL, " +
+                    "gameID INT NOT NULL PRIMARY KEY, " +
+                    "whiteUsername VARCHAR(255), " +
+                    "blackUsername VARCHAR(255), " +
+                    "gameName VARCHAR(255), " +
+                    "game JSON" + ")";
+            try (var preparedStatement = conn.prepareStatement(createGamesTable)) {
+                preparedStatement.executeUpdate();
+            }
+
+            // Create users table
+            String createUsersTable = "CREATE TABLE IF NOT EXISTS users (" +
+                    "ID INT NOT NULL PRIMARY KEY, " +
+                    "username VARCHAR(255), " +
+                    "password VARCHAR(255), " +
+                    "email VARCHAR(255)" + ")";
+            try (var preparedStatement = conn.prepareStatement(createUsersTable)){
+                preparedStatement.executeUpdate();
+            }
+
+            // Create auth table
+            String createAuthTable = "CREATE TABLE IF NOT EXISTS auth (" +
+                    "ID INT NOT NULL PRIMARY KEY, " +
+                    "authToken VARCHAR(255), " +
+                    "username VARCHAR(255)" + ")";
+            try (var preparedStatement = conn.prepareStatement(createAuthTable)){
+                preparedStatement.executeUpdate();
+            }
+
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
@@ -60,8 +97,8 @@ public class DatabaseManager {
      */
     static Connection getConnection() throws DataAccessException {
         try {
-            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
-            conn.setCatalog(DATABASE_NAME);
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
+            conn.setCatalog(databaseName);
             return conn;
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
