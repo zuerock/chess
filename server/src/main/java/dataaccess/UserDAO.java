234a4e4ba75438a979a2684fb1c7d6c1aae4b83d
zuerock <tyler.adams@byu.edu>
2024-06-04 12:18:09 -0600

    Phase 4.3

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,16 +1,11 @@
 package dataaccess;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import model.UserData;
-public class UserDAO {
-    public List<UserData> userList = new ArrayList<>();
-    public void createUser(UserData newUser){
-        userList.add(newUser);
-    }
 
-    public void clearUserList(){
-        userList.clear();
-    }
+public interface UserDAO {
+    void createUser(UserData newUser);
+    void removeUser(int index);
+    model.UserData getUser(int index);
+    int getSize();
+    void clearUserList();
 }
