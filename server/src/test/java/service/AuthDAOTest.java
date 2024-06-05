f02f4217be2e93644e94b8f717a2e2d719c53e7f
zuerock <tyler.adams@byu.edu>
2024-06-04 18:21:58 -0600

    fake

---
+++ server/src/test/java/service/AuthDAOTest.java
@@ -0,0 +1,24 @@
+package service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AuthDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createAuth() {
+    }
+
+    @Test
+    void clearAuthList() {
+    }
+}
