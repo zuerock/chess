7f72cd5f619cca03fe76f04473ca56477e884bcb
zuerock <tyler.adams@byu.edu>
2024-06-26 13:27:49 -0600

    phase 3 submit

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -9,23 +9,7 @@ public class GameDAO {
     public List<GameData> gameList = new ArrayList<>();
     public int currentID = 1;
 
-    public void createGame(GameData game){};
-
-    public int getCurrentID(){return currentID;};
-
-    public void setCurrentID(int i){};
-
-    public void removeGame(int index){};
-
-    public GameData getGame(int index){return gameList.get(index);};
-
-    public void setGame(int index, GameData game){};
-
-    public int getSize(){return gameList.size();};
-
     public void clearGameList(){
         gameList.clear();
     }
-
-    public List<GameData> returnGameList(){return gameList;};
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,22 +1,14 @@
 package dataaccess;
 
-
-import model.UserData;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import model.UserData;
 public class UserDAO {
     public List<UserData> userList = new ArrayList<>();
-
-    public void createUser(UserData user){
-        userList.add(user);
+    public void createUser(UserData newUser){
+        userList.add(newUser);
     }
-    public void deleteUser(UserData user){
-        userList.remove(user);
-    }
-
-    List<UserData> getUser(){return userList;}
 
     public void clearUserList(){
         userList.clear();
