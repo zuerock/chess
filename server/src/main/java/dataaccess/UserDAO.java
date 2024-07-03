a99c13a594bee4c84a5cb1e8c408e7277af1a4b3
zuerock <tyler.adams@byu.edu>
2024-07-03 14:35:32 -0600

    reset

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,14 +1,22 @@
 package dataaccess;
 
+
+import model.UserData;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import model.UserData;
 public class UserDAO {
     public List<UserData> userList = new ArrayList<>();
-    public void createUser(UserData newUser){
-        userList.add(newUser);
+
+    public void createUser(UserData user){
+        userList.add(user);
     }
+    public void deleteUser(UserData user){
+        userList.remove(user);
+    }
+
+    List<UserData> getUser(){return userList;}
 
     public void clearUserList(){
         userList.clear();
